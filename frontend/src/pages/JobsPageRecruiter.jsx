ed46c7c92f5a945abd08bc6a640ef01c8351c21b
rajatlko13 <rajatlko13@gmail.com>
2020-11-17 01:33:07 +0530

    Implemented Search Bar

--- frontend/src/pages/JobsPageRecruiter.jsx
+++ frontend/src/pages/JobsPageRecruiter.jsx
@@ -7,7 +7,9 @@ import axios from 'axios';
 
 class JobsPageRecruiter extends Component {
     state = { 
-        jobList: []
+        jobList: [],
+        filteredJobList: [],
+        search: ''
      }
 
     async componentDidMount() {
@@ -15,7 +17,7 @@ class JobsPageRecruiter extends Component {
             const { recruiterId } = jwtDecode(localStorage.getItem('token'));
             const jobList = await axios.get("http://localhost:9000/api/jobs/" + recruiterId);
             console.log(jobList.data.jobs);
-            this.setState({ jobList: jobList.data.jobs });
+            this.setState({ jobList: jobList.data.jobs, filteredJobList: jobList.data.jobs });
 
         } catch (error) {
             console.log(error);
@@ -23,10 +25,10 @@ class JobsPageRecruiter extends Component {
     }
 
     renderJobList = () => {
-        const list = this.state.jobList.map((job,index) => {
+        const list = this.state.filteredJobList.map((job,index) => {
             return (
                 <div className="col-lg-4 my-3" key={index} >
-                    <Card>
+                    <Card className="mx-auto">
                         <Card.Content key={job.title} header={job.title} />
                         <Card.Content key={job.description} description={job.description} />
                         <Card.Content key={index} extra>
@@ -41,15 +43,31 @@ class JobsPageRecruiter extends Component {
         return list;
     }
 
+    searchJob = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        const filteredJobList = this.state.jobList.filter( job => job.title.toLowerCase().indexOf(value.toLowerCase().trim())>=0);
+        this.setState({ filteredJobList, search: value });
+    }
+
     render() { 
         return ( 
             <div className="container mt-3">
                 <Link to="/recruiter/newJob">
                     <Button color='green' size='small' className='px-2'><Icon name="add" className="px-1" />Post New Job</Button>
                 </Link>
-                <div className="row container mx-auto mt-3">
-                    { this.state.jobList.length? this.renderJobList() : <h1>No Jobs Posted</h1>}
+                <div class="input-group mt-4">
+                    <input className="form-control border-white" type='search' name='search' id='search' placeholder="Search" value={this.state.search} onChange={this.searchJob} autoComplete='off' />
+                    <div class="input-group-append">
+                        <span class="input-group-text bg-white border border-white"><Icon name='search' className="mb-1 ml-2" /></span>
+                    </div>
                 </div>
+                
+                {this.state.filteredJobList.length>0 ?
+                    <div className="row mx-auto mt-3">
+                        { this.state.jobList.length? this.renderJobList() : <h1>No Jobs Posted</h1>}
+                    </div> : <h2>No Job Found</h2>
+                }
             </div>
          );
     }
